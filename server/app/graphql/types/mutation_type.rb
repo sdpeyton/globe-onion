e9f379322e238939d3f1c1e74e427e5bef30403f
sdpeyton <steve.peyton@arcadiapower.com>
2021-05-08 12:24:13 -0400

    remove unused graphql files (autogenerated)

--- server/app/graphql/types/mutation_type.rb
+++
@@ -1,9 +0,0 @@
-module Types
-  class MutationType < Types::BaseObject
-    # Here is where we would define our top-level mutations.
-    #
-    # There are no mutations for this exercise so this is unnecessary
-    # but since graphql schemas normally do have mutations I
-    # left it here assuming future product requirements.
-  end
-end
